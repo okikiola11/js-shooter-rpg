1f09e6680eb0c8f525383d5aea83899a11f5243f
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-30 11:06:42 +0000

    Fix eslint errors

--- src/Objects/apiScore.js
+++ src/Objects/apiScore.js
@@ -1,4 +1,4 @@
-import config from '../../config';
+import config from '../Objects/config';
 
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 const key = config.API_KEY;
